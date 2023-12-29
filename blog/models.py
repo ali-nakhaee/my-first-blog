0cc19a516f3a2d8864d2d33dfa2ca8a63339bda1
Ali <ali.n.7528@gmail.com>
2023-12-29 13:31:29 +0330

    default value for post tag changed

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    post_tag = models.CharField(max_length=50, default='main')
+    post_tag = models.CharField(max_length=50, default='')
 
     def publish(self):
         self.published_date = timezone.now()
